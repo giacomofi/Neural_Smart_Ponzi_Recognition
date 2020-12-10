ae28deda78fcc1e7e8e6e94a46b4d8f40bf36f9c
giacomofi <giacomo96ibba@outlook.it>
2020-12-10 19:00:50 +0100

    Updating paths

--- Ast Management/ast_gen_nps.py
+++ Ast Management/ast_gen_nps.py
@@ -2,25 +2,31 @@
 # 27-10-20
 
 from solidity_parser import parser
+import os
 from os import listdir
 from os.path import isfile, join
 import json
 
 # I list all my Ponzies inside the directory
+if __name__ == '__main__':
 
-not_ponzies = [f for f in listdir('/home/giacomo/PycharmProjects/ast_generator/Not Smart Ponzi Source Code') if isfile(join('/home/giacomo/PycharmProjects/ast_generator/Not Smart Ponzi Source Code', f))]
-defective_items = []
-# Process all contracts and extracts all AST
-for contract in not_ponzies:
-    contract_divide = contract.split('.')
-    contract_name = contract_divide[0]
-    ast_name = contract_name + 'AST.json'
+    this_dir, _ = os.path.split(__file__)
+    data_dir = this_dir.replace('Ast Management', 'Not Smart Ponzi Source Code')
+    not_smart_ponzi_ast_dir = this_dir.replace('Ast Management', 'Not Smart Ponzi Ast')
 
-    with open('/home/giacomo/Scrivania/AST Not Ponzi/' + str(ast_name),'w') as json_file:
-        try:
-            json.dump(parser.parse_file('/home/giacomo/PycharmProjects/ast_generator/Not Smart Ponzi Source Code/' + str(contract)),json_file)
-        except TypeError:
-            defective_items.append(contract)
-        except AttributeError:
-            defective_items.append(contract)
+    not_ponzies = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
+    defective_items = []
+    # Process all contracts and extracts all AST
+    for contract in not_ponzies:
+        contract_divide = contract.split('.')
+        contract_name = contract_divide[0]
+        ast_name = contract_name + 'AST.json'
+
+        with open(not_smart_ponzi_ast_dir + '/' + str(ast_name), 'w') as json_file:
+            try:
+                json.dump(parser.parse_file(data_dir + '/' + str(contract)), json_file)
+            except TypeError:
+                defective_items.append(contract)
+            except AttributeError:
+                defective_items.append(contract)
 
--- Ast Management/astgen_ps.py
+++ Ast Management/astgen_ps.py
@@ -5,19 +5,22 @@ from solidity_parser import parser
 from os import listdir
 from os.path import isfile, join
 import json
+import os
 
-#I list all my Ponzies inside the directory
+if __name__ == '__main__':
 
-ponzies = [f for f in listdir('/home/giacomo/PycharmProjects/ast_generator/Smart Ponzi') if isfile(join('/home/giacomo/PycharmProjects/ast_generator/Smart Ponzi', f))]
+    # I list all my Ponzies inside the directory
+    this_dir, _ = os.path.split(__file__)
+    data_dir = this_dir.replace('Ast Management', 'Smart Ponzi')
+    smart_ponzi_ast_dir = this_dir.replace('Ast Management', 'Ponzi Abstract Syntax Trees')
 
+    ponzies = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
 
-#Process all contracts and extracts all AST
-for contract in ponzies:
-    contract_divide = contract.split('.')
-    contract_name = contract_divide[0]
-    ast_name = contract_name + 'AST.json'
+    # Process all contracts and extracts all AST
+    for contract in ponzies:
+        contract_divide = contract.split('.')
+        contract_name = contract_divide[0]
+        ast_name = contract_name + 'AST.json'
 
-    with open('/home/giacomo/Scrivania/AST/' + str(ast_name),'w') as json_file:
-        json.dump(parser.parse_file('/home/giacomo/PycharmProjects/ast_generator/Smart Ponzi/' + str(contract)),json_file)
-
-
+        with open(smart_ponzi_ast_dir + '/' + str(ast_name), 'w') as json_file:
+            json.dump(parser.parse_file(data_dir + '/' + str(contract)), json_file)
