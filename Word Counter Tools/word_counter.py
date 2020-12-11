d512488e68bc877ffd58381d010bec6496bc0fc1
giacomofi <giacomo96ibba@outlook.it>
2020-12-11 11:46:34 +0100

    Updated build_semantic for txt files

--- Word Counter Tools/word_counter.py
+++ Word Counter Tools/word_counter.py
@@ -1,18 +1,17 @@
 from pyspark import SparkContext, SparkConf
-from os import listdir
-from os.path import isfile, join, abspath
-import sys
+import os
 
 if __name__ == '__main__':
     # Setting Spark context
     conf = SparkConf().setAppName("Word Counter")
     sc = SparkContext(conf=conf)
     sc.setLogLevel("ERROR")
-
-    words = sc.textFile('/home/giacomo/PycharmProjects/ast_generator/Docs/Ponzi Semantic Documents/*.txt').flatMap(lambda line: line.split(' '))
-    word_counts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b).top(50, key= lambda x: x[1])
+    this_dir, _ = os.path.split(__file__)
+    data_dir = this_dir.replace('Word Counter Tools', 'Docs')
+    words = sc.textFile(data_dir + '/Ponzi Semantic Documents/*.txt').flatMap(lambda line: line.split(' '))
+    word_counts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b).top(50, key=lambda x: x[1])
     try:
-        with open('/home/giacomo/PycharmProjects/ast_generator/Docs/Most used terms in Ponzies.txt', 'w') as f:
+        with open(data_dir + '/Most used terms in Ponzies.txt', 'w') as f:
             for word in word_counts:
                 f.write(word[0] + ' ' + str(word[1]))
                 f.write('\n')
